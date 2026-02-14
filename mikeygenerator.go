f0f0727f21aa1fb7085e1b5bf068168e79145c58
mugomes <murilogomes5792@gmail.com>
2026-02-14 14:06:03 -0300

    Alterado o copyright e removido o gerador de hash

--- mikeygenerator.go
+++ mikeygenerator.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Murilo Gomes Julio
+// Copyright (C) 2025-2026 Murilo Gomes Julio
 // SPDX-License-Identifier: GPL-2.0-only
 
 // Site: https://mugomes.github.io
@@ -6,12 +6,6 @@
 package main
 
 import (
-	"crypto/md5"
-	"crypto/sha1"
-	"crypto/sha256"
-	"crypto/sha512"
-	"fmt"
-	"image/color"
 	"math/rand"
 	"net/url"
 	"strings"
@@ -22,45 +16,10 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
-const VERSION_APP string = "6.1.0"
-
-type myDarkTheme struct{}
-
-func (m myDarkTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
-	// A lógica para forçar o modo escuro é retornar cores escuras.
-	// O Fyne usa estas constantes internamente:
-	switch name {
-	case theme.ColorNameBackground:
-		return color.RGBA{28, 28, 28, 255} // Fundo preto
-	case theme.ColorNameForeground:
-		return color.White // Texto branco
-	// Adicione outros casos conforme a necessidade (InputBackground, Primary, etc.)
-	default:
-		// Retorna o tema escuro padrão para as outras cores (se existirem)
-		// Aqui estamos apenas definindo as cores principais para garantir o Dark Mode
-		return theme.DefaultTheme().Color(name, theme.VariantDark)
-	}
-}
-
-// 3. Implemente os outros métodos necessários da interface fyne.Theme (usando o tema padrão)
-func (m myDarkTheme) Font(s fyne.TextStyle) fyne.Resource {
-	return theme.DefaultTheme().Font(s)
-}
-
-func (m myDarkTheme) Icon(n fyne.ThemeIconName) fyne.Resource {
-	return theme.DefaultTheme().Icon(n)
-}
-
-func (m myDarkTheme) Size(n fyne.ThemeSizeName) float32 {
-	if n == theme.SizeNameText {
-		return 16
-	}
-	return theme.DefaultTheme().Size(n)
-}
+const VERSION_APP string = "1.1.0"
 
 func generateKey(maiuscula bool, minuscula bool, numeros bool, caracteresespeciais bool, tamanhosegmento int, tamanhochave int) string {
 	var retorno, caracteres string
@@ -105,31 +64,16 @@ func generateKey(maiuscula bool, minuscula bool, numeros bool, caracteresespecia
 		}
 	}
 
-	// retorno[:len(retorno)-1]
 	return strings.TrimRight(retorno, "-")
 }
 
-func generateHash(tipo string, valor string) string {
-	var sHash string
-
-	if tipo == "" {
-		sHash = fmt.Sprintf("%x", md5.Sum([]byte(valor)))
-	} else if tipo == "MD5" {
-		sHash = fmt.Sprintf("%x", md5.Sum([]byte(valor)))
-	} else if tipo == "SHA1" {
-		sHash = fmt.Sprintf("%x", sha1.Sum([]byte(valor)))
-	} else if tipo == "SHA256" {
-		sHash = fmt.Sprintf("%x", sha256.Sum256([]byte(valor)))
-	} else if tipo == "SHA512" {
-		sHash = fmt.Sprintf("%x", sha512.Sum512([]byte(valor)))
-	}
-
-	return sHash
-}
-
 func main() {
+	sIcon := fyne.NewStaticResource("mikeygenerator.png", resourceAppIconPngData)
+
 	app := app.NewWithID("br.com.mugomes.mikeygenerator")
 	app.Settings().SetTheme(&myDarkTheme{})
+	app.SetIcon(sIcon)
+
 	window := app.NewWindow("MiKeyGenerator")
 	window.SetFixedSize(true)
 	window.CenterOnScreen()
@@ -146,12 +90,12 @@ func main() {
 
 	mnuAbout := fyne.NewMenu("Sobre",
 		fyne.NewMenuItem("Verificar Atualização", func() {
-			url, _ := url.Parse("https://www.mugomes.com.br/p/mikeygenerator.html")
+			url, _ := url.Parse("https://github.com/mugomes/mikeygenerator/releases")
 			app.OpenURL(url)
 		}),
 		fyne.NewMenuItemSeparator(),
 		fyne.NewMenuItem("Apoie MiKeyGenerator", func() {
-			url, _ := url.Parse("https://www.mugomes.com.br/p/apoie.html")
+			url, _ := url.Parse("https://mugomes.github.io/apoie.html")
 			app.OpenURL(url)
 		}),
 		fyne.NewMenuItemSeparator(),
@@ -170,11 +114,6 @@ func main() {
 	chkOptions.Horizontal = false
 
 	flow := mgsmartflow.New()
-	lblTipoHash := widget.NewLabel("Tipo de Hash")
-	lblTipoHash.TextStyle = fyne.TextStyle{Bold: true}
-	cboTipoHash := widget.NewSelect([]string{"MD5", "SHA1", "SHA256", "SHA512"}, nil)
-	cboTipoHash.PlaceHolder = "MD5"
-
 	lblTamanho := widget.NewLabel("Tamanho")
 	lblTamanho.TextStyle = fyne.TextStyle{Bold: true}
 
@@ -199,7 +138,6 @@ func main() {
 	flow.AddColumn(
 		container.NewVBox(lblBox, chkBox),
 		container.NewVBox(
-			lblTipoHash, cboTipoHash,
 			lblTamanho,
 			container.NewHBox(
 				container.NewVBox(lblTamanhoSegmento, txtTamanhoSegmento),
@@ -231,31 +169,20 @@ func main() {
 		}
 		txtChave.Text = generateKey(sMaiuscula, sMinuscula, sNumeros, sCaracteresEspeciais, rTamanhoSegmento.GetValue(), rTamanhoChave.GetValue())
 		txtChave.Refresh()
-
-		txtHash.Text = generateHash(cboTipoHash.Selected, txtChave.Text)
-		txtHash.Refresh()
-
 	})
 
 	flow.AddRow(btnGerar)
-	flow.SetResize(btnGerar, fyne.NewSize(150, 50))
-	flow.SetMove(btnGerar, fyne.NewPos(window.Canvas().Size().Width/3.1, 0))
-	flow.SetGap(btnGerar, fyne.NewPos(0, 17))
+	flow.Resize(btnGerar, 150, 50)
+	flow.Move(btnGerar, window.Canvas().Size().Width/3.1, 0)
+	flow.Gap(btnGerar, 0, 17)
 
 	lblChave := widget.NewLabel("Chave")
 	lblChave.TextStyle = fyne.TextStyle{Bold: true}
 	txtChave = widget.NewEntry()
 
-	lblHash := widget.NewLabel("Hash")
-	lblHash.TextStyle = fyne.TextStyle{Bold: true}
-	txtHash = widget.NewEntry()
-
-	flow.AddColumn(
-		container.NewVBox(lblChave, txtChave),
-		container.NewVBox(lblHash, txtHash),
-	)
+	flow.AddRow(lblChave)
+	flow.AddRow(txtChave)
 
 	window.SetContent(flow.Container)
 	window.ShowAndRun()
-
 }
